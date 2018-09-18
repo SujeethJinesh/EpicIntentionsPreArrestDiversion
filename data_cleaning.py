172e0dc9280903a35c45dea2083c27a5c1398b9d
SujeethJinesh <sujeethjinesh@gmail.com>
2018-09-18 17:57:45 -0400

    Update Lat/Lngs

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from api_keys import GOOGLE_MAPS_API_KEY
 from pandas import ExcelWriter
+from math import isnan
 
 # location of data
 file_name = "data/09.13.18 PAD Diversions To Date.xlsx"
@@ -11,21 +12,25 @@ excel = pd.read_excel(io=file_name)
 addresses = excel['LOCATION/STREET'].tolist()
 
 for index, address in enumerate(addresses):
+    # if address == "10 WILLIAM ST":
+    #     import ipdb; ipdb.set_trace()
     address += ", Atlanta, GA"
     req = requests.get(GOOGLE_MAPS_API_URL + address.replace(" ", "+"))
     resp = req.json()
-    lat_lng = resp['results'][0]['geometry']['location']
-    print(address)
-    lat = lat_lng['lat']
-    lng = lat_lng['lng']
-    print(lat_lng)
-
     # import ipdb; ipdb.set_trace()
+    try:
+        lat_lng = resp['results'][0]['geometry']['location']
+        print(address)
+        lat = lat_lng['lat']
+        lng = lat_lng['lng']
+        print(lat_lng)
 
-    # X is longitude and Y is latitude
-    if excel['X'][index] == '--' or excel is None or excel == '':
-        excel['X'][index] = lng
-        excel['Y'][index] = lat
+        # X is longitude and Y is latitude
+        if excel['X'][index] == '--' or excel['X'][index] is None or excel['X'][index] == '' or isnan(excel['X'][index]):
+            excel['X'][index] = lng
+            excel['Y'][index] = lat
+    except IndexError:
+        pass
 
 writer = ExcelWriter("CleanedData.xlsx")
 excel.to_excel(writer, 'Sheet1')
